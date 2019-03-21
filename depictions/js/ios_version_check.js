785be405112837c2af564f3c84ba6012ecc962e5
ArcherInnovations <48256101+ArcherInnovations@users.noreply.github.com>
2019-03-21 03:29:55 -0700

    experiments

--- depictions/js/ios_version_check.js
+++ depictions/js/ios_version_check.js
@@ -46,7 +46,7 @@ Twitter - @TimonOlsthoorn
 
 
 // changed const to var for IE9/10 compatibity.
-var VERSION_CHECK_SUPPORTED = "Your iOS version is supported!            ✅";
+var VERSION_CHECK_SUPPORTED = "Your iOS version is supported!" + <style="text-align: right;">"✅"</style>;
 var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
 var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
 var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
